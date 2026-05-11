c26af0776ca0d06bca37c66854a8cb6f10b2251c
Reito <100802000+reitoserizawa@users.noreply.github.com>
2026-05-11 12:35:56 -0400

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # x^n = (x^(n//2))^2 if n is even else x * (x^(n//2))^2
+
+        def calculatePow(a, b):
+            if b == 0:
+                return 1
+            
+            half = calculatePow(a, b // 2)
+
+            if b % 2 == 0:
+                return half * half
+            return half * half * a
+        
+        if n < 0:
+            x = 1 / x
+            n = -n
+
+        return calculatePow(x, n)
