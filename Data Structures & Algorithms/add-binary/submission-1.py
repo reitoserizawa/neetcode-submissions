91553115e672ee0b111dccf7410239efd13ea900
Reito <100802000+reitoserizawa@users.noreply.github.com>
2026-05-13 14:09:57 -0400

    Add: add-binary - submission-1

---
+++ Data Structures & Algorithms/add-binary/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        x = int(a, 2)
+        y = int(b, 2)
+
+        while y:
+            carry = (x & y) << 1
+            x = x ^ y
+            y = carry
+
+        return bin(x)[2:]
