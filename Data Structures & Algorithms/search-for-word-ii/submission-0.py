177a17b8bdf725da634daf4cbb45ff1a7cfbf68a
Reito <100802000+reitoserizawa@users.noreply.github.com>
2026-07-08 11:31:27 -0400

    Add: search-for-word-ii - submission-0

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-0.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = Node()
+
+        for w in words:
+            cur = root
+            for l in w:
+                if l not in cur.children:
+                    cur.children[l] = Node()
+                cur = cur.children[l]
+            cur.isEnd = True
+        
+        ROWS, COLS = len(board), len(board[0])
+        DIRS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        res = []
+
+        def dfs(r, c, cur, w, visited):
+            if not (0 <= r < ROWS) or not (0 <= c < COLS):
+                return
+            
+            if board[r][c] not in cur.children:
+                return
+            
+            if (r, c) in visited:
+                return
+            
+            w.append(board[r][c])
+            visited.add((r, c))
+            cur = cur.children[board[r][c]]
+
+            if cur.isEnd:
+                res.append(''.join(w))
+                cur.isEnd = False
+            
+            for dr, dc in DIRS:
+                nr, nc = r+dr, c+dc
+                dfs(nr, nc, cur, w, visited)
+            
+            w.pop()
+            visited.remove((r, c))
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, root, [], set())
+        
+        return res
+
+
+        
