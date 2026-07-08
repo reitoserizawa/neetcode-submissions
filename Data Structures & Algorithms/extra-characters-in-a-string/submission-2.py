e1412140b9c564b6141886616e27b187bb294338
Reito <100802000+reitoserizawa@users.noreply.github.com>
2026-07-08 11:13:20 -0400

    Add: extra-characters-in-a-string - submission-2

---
+++ Data Structures & Algorithms/extra-characters-in-a-string/submission-2.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        self.trie = Node()
+
+        for word in dictionary:
+            cur = self.trie
+            for l in word:
+                if l not in cur.children:
+                    cur.children[l] = Node()
+                cur = cur.children[l]
+            cur.isEnd = True
+
+        n = len(s)
+        dp = [0] * (n+1)
+        dp[-1] = 0
+        
+        for i in range(n-1, -1, -1):
+            dp[i] = dp[i+1] + 1
+            cur = self.trie
+
+            for j in range(i, n):
+                if s[j] not in cur.children:
+                    break
+                cur = cur.children[s[j]]
+                if cur.isEnd:
+                    dp[i] = min(dp[i], dp[j+1])
+
+
+        return dp[0]
